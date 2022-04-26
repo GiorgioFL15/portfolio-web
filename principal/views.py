c149ac218aece37e78f6670945768e78dd665e6d
GiorgioFL15 <frigottogiorgio@gmail.com>
2022-04-26 14:02:17 -0300

    Adding new functions in portfolio project, page portfolio, sobre and form for budget

--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import get_object_or_404, render
 def index(request):
     return render(request, 'index.html')
 
+def portfolio(request):
+    return render(request, 'pages/portfolio.html')
+
+def sobre(request):
+    return render(request, 'pages/sobre.html')
