b9f0ba93883ffdf48b9b3061787e15ea014860c4
GiorgioFL15 <frigottogiorgio@gmail.com>
2022-04-27 15:28:05 -0300

    Adding robot to send emails and real projects

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=300)
+    phone = models.CharField(max_length=13)
+    site= models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,48 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from pydoc import describe
+import smtplib
 from django.shortcuts import get_object_or_404, render
+from principal.models import Form
+from smtplib import SMTP
 
 def index(request):
+
+    if request.method == 'POST':
+        name = request.POST['Nome']
+        email = request.POST['Email']
+        phone = request.POST['Telefone']
+        site = request.POST['Site']
+        description = request.POST['Mensagem']
+
+        principal = Form.objects.create(name=name, email=email,
+        phone=phone, site=site, description=description)
+
+        principal.save()
+
+        send_email(email, name)
     return render(request, 'index.html')
+
+def send_email(to: str, name: str,):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        # DESATIVAR MODO DE SEGURANÇA DA CONTA
+        server.ehlo()
+        server.starttls() # start transport layer security
+        server.login('frigottogiorgio@gmail.com', '96599685gg')
+        # ESTRUTURA DA ENVIO DE MSG
+        msg_text = f"""<h3>Que incrivel!! Vejo que se interessou pelo meu trabalho!</h3>
+        <p>Olá {name}, Tudo bem com você?</p>
+        <p>Verificamos seu interesse, em meus trabalhos!</p>
+        <p>Assim que possível lhe enviamos um orçamento desse íncrivel sistema!</p>
+        <p>Caso queira entrar diretamente em contato comigo, Segue meu whatsapp +55 48 9 9935 6645</p>"""
+        msg = MIMEMultipart()
+        msg['From'] = 'frigottogiorgio@gmail.com'
+        msg['To'] = to
+        msg['Subject'] = 'Agradecimento!!'
+        msg.attach(MIMEText(msg_text, 'html'))
+        # FROM > TO > MSG
+        server.sendmail('frigottogiorgio@gmail.com', to, msg.as_string())
+        server.quit()
 
 def portfolio(request):
     return render(request, 'pages/portfolio.html')
